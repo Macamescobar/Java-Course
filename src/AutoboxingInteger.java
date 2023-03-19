095e77b483f84bcfc5fb99bb685a27b8a9db81c6
Macamescobar <108288476+Macamescobar@users.noreply.github.com>
2023-03-19 20:28:08 -0300

    clases wrapper

---
+++ src/AutoboxingInteger.java
@@ -0,0 +1,16 @@
+public class AutoboxingInteger {
+    public static void main(String[] args) {
+
+        // suma solo de numeros pares
+        Integer[] enteros = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+        
+        int suma = 0;
+        
+        for( Integer i : enteros ){
+            if( i % 2 == 0 ){
+                suma += i ;
+            }
+        }
+        System.out.println("suma = " + suma);
+    }
+}
